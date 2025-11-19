f9fccd18929e6213c04f439954053d2cad2b4e4a
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-11-19 12:23:15 +0100

    fixed socials with burger menu on smartphone

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,24 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const Socials = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <div className="container flex flex-row items-center justify-end -mb-16">
-        <div className="h-8 w-8 md:w-10 md:h-10 flex flex-col mt-2 justify-around items-center
-        hover:scale-110 transition-all ease-in-out cursor-pointer">
-          <img src="/socials/git.png" alt="Github" href="https://github.com/Gus-gallery"/>
+    <div className="relative flex items-center justify-end opacity-70">
+
+      <div className="container hidden md:flex flex-row items-center justify-end gap-2 -mt-2 -mb-16">
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://github.com/Gus-gallery" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/git.png" alt="Github"/>
+            </a>
+          </div>
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://github.com/Gus-gallery" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/mail.png" alt="E-mail"/>
+            </a>
+          </div>    
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://www.linkedin.com/in/gustav-werdelin-677785394/" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/link.png" alt="LinkedIn"/>
+            </a>
+          </div>
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://www.instagram.com/gustavwerdelin/" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/insta.png" alt="Instagram"/>
+            </a>
+          </div>
+      </div>
+
+      <button className="md:hidden flex items-center justify-end p-2"
+      onClick={() => setOpen(!open)}>
+        <img src="/socials/menu.svg" alt="menu" className={`h-8 w-8 transition-transform duration-300 ${open ? "rotate-90 opacity-50" : ""}`}>
+        </img>
+      </button>
+
+      {open && (
+        <div className="flex flex-row items-center justify-end gap-2 md:hidden">
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://github.com/Gus-gallery" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/git.png" alt="Github"/>
+            </a>
+          </div>
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://github.com/Gus-gallery" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/mail.png" alt="E-mail"/>
+            </a>
+          </div>    
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://www.linkedin.com/in/gustav-werdelin-677785394/" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/link.png" alt="LinkedIn"/>
+            </a>
+          </div>
+          <div className="h-8 w-8 md:w-10 md:h-10 justify-around items-center
+          hover:scale-110 transition-all ease-in-out cursor-pointer">
+            <a href="https://www.instagram.com/gustavwerdelin/" target="_blank" rel="noopener noreferrer">
+              <img src="/socials/insta.png" alt="Instagram"/>
+            </a>
+          </div>
         </div>
-        <div className="h-8 w-8 md:w-10 md:h-10 flex flex-col mt-2 justify-around items-center
-        hover:scale-110 transition-all ease-in-out cursor-pointer">
-          <img src="/socials/mail.png" alt="Mail"/>
-        </div>    
-        <div className="h-8 w-8 md:w-10 md:h-10 flex flex-col mt-2 justify-around items-center
-        hover:scale-110 transition-all ease-in-out cursor-pointer">
-          <img src="/socials/link.png" alt="LinkedIn"/>
-        </div>
-        <div className="h-8 w-8 md:w-10 md:h-10 flex flex-col mt-2 justify-around items-center
-        hover:scale-110 transition-all ease-in-out cursor-pointer">
-          <img src="/socials/insta.png" alt="Instagram"/>
-        </div>
+      )}
     </div>
   )
 }
