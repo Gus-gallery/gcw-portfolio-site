7c3c61747a09318b96489999c1cc98a0a1eab88f
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-11-20 12:54:47 +0100

    Further improvements to responsivity: included XL responsiveness.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,12 +4,12 @@ const Hero = () => {
   return (
     <section id="hero">
       <div>
-        <div className="container flex flex-col p-4 mt-52 sm:mt-68 md:mt-78 lg:mt-78 text-left xl:max-w-1/2">
+        <div className="container flex flex-col p-4 mt-72 sm:mt-82 lg:mt-78 xl:mt-86 text-left lg:max-w-1/2">
           <img src="hero_bg.png" alt="Hero bg" className="absolute top-0 left-0 object-bottom h-132 w-full object-cover opacity-25 shadow-lg"/>
 
           <div className="relative z-10">
             <h1 className="text-lg md:text-2xl font-bold text-primary">
-              Hi, I'm <span className="text-blue-200">Gustav C. Werdelin,</span> <br /> an art historian and front-end developer.
+              <span className="text-blue-200">Gustav C. Werdelin</span>'s development portfolio.
             </h1>
             <p className="text-md md:text-lg font-regular text-primary">
               I'm a former gallerist with a bachelor's degree in art history, 
