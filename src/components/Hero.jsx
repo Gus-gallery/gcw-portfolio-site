56910c4b19d65cdd54551d5eef0930704caaaad4
Goose <207717976+Gus-gallery@users.noreply.github.com>
2025-11-20 12:37:03 +0100

    layout responsive hero section... Attempt #2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <section id="hero">
       <div>
-        <div className="container flex flex-col p-4 mt-40 md:mt-66 text-left xl:max-w-1/2">
-          <img src="hero_bg.png" alt="Hero bg" className="absolute top-0 left-0 object-bottom h-120 xs:h-128 sm:h-100 md:h-120 w-full object-cover opacity-25 shadow-lg"/>
+        <div className="container flex flex-col p-4 mt-52 sm:mt-68 md:mt-78 lg:mt-78 text-left xl:max-w-1/2">
+          <img src="hero_bg.png" alt="Hero bg" className="absolute top-0 left-0 object-bottom h-132 w-full object-cover opacity-25 shadow-lg"/>
 
           <div className="relative z-10">
             <h1 className="text-lg md:text-2xl font-bold text-primary">
