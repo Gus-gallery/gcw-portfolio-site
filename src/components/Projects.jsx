7f6df5e7d25950fd5cf560b941b535a5aefc7dcf
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-01-19 22:53:52 +0100

    Trying to fix commit huistory again

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -92,15 +92,30 @@ const VideoCard = ({ videoSrc, link, title, description }) => {
 
 const Projects = () => {
   const videoRef = useRef(null);
+  const projectRef = useRef(null);
 
   useGSAP(() => {
     gsap.from(videoRef.current, {
-      x: screen.width * 0.2,
+      x: screen.width * 0.3,
       opacity: 0,
       ease: "easeInOut",
-      duration: 1,
+      duration: 1.5,
       scrollTrigger: {
         trigger: videoRef.current,
+        start: "top 80%",
+        once: true,
+      }
+    });
+  }, []);
+
+  useGSAP(() => {
+    gsap.from(projectRef.current, {
+      y: 50,
+      opacity: 0,
+      ease: "easeInOut",
+      duration: 1.5,
+      scrollTrigger: {
+        trigger: projectRef.current,
         start: "top 80%",
         once: true,
       }
@@ -110,8 +125,23 @@ const Projects = () => {
   return (
     <section id="projects">
       <div className="mt-12 p-4">
-        <h1 className="text-xl mb-2 text-primary font-medium">Recent Projects</h1>
+        <h1 className="text-xl mb-2 text-left text-primary font-medium">Current project</h1>
 
+        <div ref={projectRef} className="flex flex-col gap-4 lg:w-full items-left">
+            <div className="bg-backgroundsecondary rounded-md p-4 gradient-border">
+              <h2 className='font-medium text-primary'>
+                Anglerfish
+              </h2>
+              <p className="font-light text-secondary text-md mb-4">
+                A project I'm currently working on, which is a social media platform for anglers to share their catches, tips, and experiences. Built with Next.js and Supabase as an easy database solution. See the project live at 
+                <a href="https://anglerfish-kappa.vercel.app" target="_blank" rel="noopener noreferrer" className='text-secondary hover:text-primary font-bold'> anglerfish-kappa.vercel.app</a>.
+              </p>
+                <img href="https://anglerfish-kappa.vercel.app" src='/projects/anglerfish.png' alt='Anglerfish project screenshot' className='border border-secondary gradient-border rounded-md cursor-pointer'>
+                </img>
+            </div>
+        </div>
+
+        <h1 className="text-xl mb-2 mt-24  text-primary font-medium">Recent Projects</h1>
         <div
           ref={videoRef}
           className="
