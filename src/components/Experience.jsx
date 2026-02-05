4c208b0a1889b4fb791d6450c2271fe8d7a032c0
Goose <207717976+Gus-gallery@users.noreply.github.com>
2026-02-05 13:02:17 +0100

    no box animation

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { useRef } from 'react'
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
-import { ScrollTrigger } from 'gsap/all';
 
 const techs = [
     {
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,39 @@
 import React from 'react'
+import { useRef } from 'react'
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
+const Box = () => (
+  <div className="w-20 h-20 bg-backgroundthird rounded-sm scale-10">
+  </div>
+)
 
 const Hero = () => {
+  const numberOfBoxes = 80; 
+  const boxRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.from(boxRef.current.children, {
+      scale: 50,
+      stagger: {
+        each: 0.1,
+        from: "center",
+        grid: "auto",
+      },
+      ease: "easeInOut",
+      duration: 3,
+    });
+  }, []);
+
   return (
     <section id="hero">
       <div>
-        <div className="container flex flex-col p-4 mt-18 mb-38 sm:mt-44 md:mt-44 md:mb-48">
-           
+        <div className="absolute container flex flex-col">
+           <div className="grid grid-cols-5 lg:grid-cols-10 gap-4" ref={boxRef}>
+            {Array.from({ length: numberOfBoxes }).map((_, i) => (
+              <Box key={i} />
+            ))}
+           </div>
         </div>
       </div>
     </section>
